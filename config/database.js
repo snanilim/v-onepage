603274f4df25bf362556d48dd60978db3df416af
snanilim <snanilim@gmail.com>
2019-07-21 17:22:30 +0600

    database add cng

--- config/database.js
+++ config/database.js
@@ -7,3 +7,14 @@ module.exports = mysql.createPool({
     password: 'admin@123',
     database: 'vtracker'
 })
+
+
+
+// module.exports = mysql.createPool({
+//     connectionLimit : 100,
+//     host : 'localhost',
+//     user :  'tuhin',
+//     password: 'tuhin@4321',
+//     database: 'vms'
+// })
+
